40dc35ad158e1752c362cfc120d2bb9844d14ee8
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-22 01:03:57 +0300

    fin

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@
 import { from } from 'rxjs';
 import { first } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/first
+// Example 3: Utilizing default value
+
 const list$ = from([1, 2, 3, 4, 5]);
 //no value will pass, emit default
 const example = list$.pipe(first((val: any) => val > 5, 'Nothing'));
